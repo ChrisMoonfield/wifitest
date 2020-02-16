d9838741e17d96499727c0489472fc959957675d
ChrisMoonfield <60901081+ChrisMoonfield@users.noreply.github.com>
2020-02-16 23:37:45 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,12 @@
 sudo sh -c "echo 'deb https://http.kali.org/kali kali-rolling main non-free contrib' > /etc/apt/sources.list.d/kali.list"
 sudo apt install gnupg wget
+cd
 wget 'https://archive.kali.org/archive-key.asc'
 sudo apt-key add archive-key.asc
 sudo apt update -y
 sudo sh -c "echo 'Package: *'>/etc/apt/preferences.d/kali.pref; echo 'Pin: release a=kali-rolling'>>/etc/apt/preferences.d/kali.pref; echo 'Pin-Priority: 50'>>/etc/apt/preferences.d/kali.pref"
 sudo apt update
+cd
 git clone https://github.com/ZerBea/hcxdumptool.git
 cd hcxdumptool
 make
