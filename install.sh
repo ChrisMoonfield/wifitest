84633957064b6371e56f42a488f4e1cd8a4a52d4
ChrisMoonfield <60901081+ChrisMoonfield@users.noreply.github.com>
2020-02-17 01:30:45 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -12,24 +12,25 @@ git clone https://github.com/ZerBea/hcxdumptool.git
 cd hcxdumptool
 sudo make
 sudo make install
-cd /root/wifitools
+cd /root/wifitest
 git clone https://github.com/ZerBea/hcxtools
 cd hcxtools
 sudo make
 sudo make install
-cd /root/wifitools
+cd /root/wifitest
 sudo apt install git python iw macchanger libcurl4-openssl-dev libssl-dev zlib1g-dev libpcap-dev net-tools original-awk mawk gawk aircrack-ng xterm beef-xss reaver isc-dhcp-server dsniff sslstrip asleap bettercap hashcat john bully ettercap-text-only mdk4 hostapd hostapd-wpe lighttpd pixiewps curl tshark cowpatty pyrit hcxdumptool nmap routersploit crunch wireless-regdb crda wpasupplicant haveged util-linux procps iproute2 dnsmasq iptables wireshark-qt sipcalc -y
 sudo apt update -y && apt upgrade -y
 git clone https://github.com/oblique/create_ap
 cd create_ap
 sudo make install
 cd .. && rm -rf create_ap
-cd /root/wifitools
+cd /root/wifitest
 git clone https://github.com/Mi-Al/WiFi-autopwner.git
 git clone --depth 1 https://github.com/v1s1t0r1sh3r3/airgeddon.git
 git clone https://github.com/derv82/wifite2.git
 cd wifite2
 python setup.py install
-cd /root/wifitools
+cd /root/wifitest
+sudo chmod 755 wifitest
 mv wifitest /usr/local/bin
 sudo rm -r install.sh
